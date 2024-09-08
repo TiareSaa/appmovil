73351064687411de73d02de04f88dbaba057a01f
fbeniscellip <164574233+fbeniscelli@users.noreply.github.com>
2024-09-08 20:10:52 -0300

    Animacion-ionic

--- src/app/bienvenida/bienvenida.page.ts
+++ src/app/bienvenida/bienvenida.page.ts
@@ -7,13 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BienvenidaPage implements OnInit {
   userName: string | null = '';
+  pokemons: Array<{ name: string; type: string; level: number; image: string }> = [];
 
   constructor() { }
 
   ngOnInit() {
     // Recuperar el nombre del usuario desde localStorage
     this.userName = localStorage.getItem('userName');
+
+    // Inicializar la lista de Pokémon con las imágenes
+    this.pokemons = [
+      { name: 'Charizard', type: 'Fuego', level: 36, image: 'assets/img/Charizard.png' },
+      { name: 'Pikachu', type: 'Eléctrico', level: 25, image: 'assets/img/Pikachu.png' },
+      { name: 'Ditto', type: 'Normal', level: 20, image: 'assets/img/Ditto.png' },
+      { name: 'Squirtle', type: 'Agua', level: 16, image: 'assets/img/Squirtle.png' },
+      { name: 'Articuno', type: 'Hielo/Volador', level: 50, image: 'assets/img/Articuno.png' }
+    ];
   }
 }
-
-
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -11,28 +11,30 @@ export class LoginPage implements OnInit {
   loginForm: FormGroup;
 
   constructor(private formBuilder: FormBuilder, private router: Router) {
+    // Configuración del formulario con validaciones
     this.loginForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
+      email: ['', [Validators.required, Validators.email]], // Valida email
       password: ['', [
-        Validators.required, 
-        Validators.minLength(8),
-        Validators.pattern(/^(?=(.*\d){4,})(?=(.*[!@#$%^&*()\-__+.]{3,}))(?=.*[A-Z]).{8,}$/)
+        Validators.required, // Requerido
+        Validators.minLength(8), // Mínimo 8 caracteres
+        Validators.pattern(/^(?=(.*\d){4,})(?=(.*[!@#$%^&*()\-__+.]{3,}))(?=.*[A-Z]).{8,}$/) // 4 números, 3 caracteres especiales, 1 mayúscula
       ]]
     });
   }
-    
+
   ngOnInit(): void {}
 
+  // Método de login
   onLogin() {
     if (this.loginForm.valid) {
       console.log('Formulario válido:', this.loginForm.value);
-      this.router.navigate(['/bienvenida']);
-      // Aquí puedes manejar el envío del formulario, como una solicitud HTTP a una API.
+      this.router.navigate(['/bienvenida']); // Redirige a la página de bienvenida si el login es exitoso
     } else {
       console.log('Formulario inválido');
     }
   }
 
+  // Método para recuperar contraseña
   recuperarContrasena() {
     console.log('Redirigiendo a recuperación de contraseña');
     this.router.navigate(['/recuperar-contrasena']); // Redirige a la página de recuperación de contraseña
